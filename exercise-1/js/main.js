436649727c9c5a7625dfd653653109fb5f6ef4de
mkfreeman <michael.k.freeman@gmail.com>
2016-03-16 19:52:52 -0700

    Fixed exercise files

--- exercise-1/js/main.js
+++ exercise-1/js/main.js
@@ -17,4 +17,4 @@ var str = "this is a sentence";
 
 // Write a function to test if a string contains a word, and returns a boolean value.
 
-// Pass a string to your function to determine that it works
+// Pass a phrase and a string to your function to determine that it works
--- exercise-2/js/main.js
+++ exercise-2/js/main.js
@@ -1,7 +1,7 @@
 // Main JavaScript File
 
 // Array to work with
-var arr1 = [1,2,3,4,5]
+var arr1 = [1,2,3,4,5];
 
 // Write a function to subtract one from a value
 
@@ -13,7 +13,7 @@ var arr1 = [1,2,3,4,5]
 
 
 // Array of strings
-var names = ['Tim', 'Nick', 'Grant']
+var names = ['Tim', 'Nick', 'Grant'];
 
 // Write an introduction function that takes an argument and returns "hello, I'm " + argument
 
@@ -22,13 +22,10 @@ var names = ['Tim', 'Nick', 'Grant']
 
 // Array of objects
 var people = [
-	{name:'Tim', age:22}, 
-	{name:'Jan', age:12}, 
+	{name:'Tim', age:22},
+	{name:'Jan', age:12},
 	{name:'Isabel', age:43}
-]
+];
 
 // Use the `.forEach` method to create a new property `intro` for each object in your people array
 // The property should say "My name is NAME and I'm AGE years old"
-people.forEach(function(d){
-	d.intro = "My name is " + d.name + " and I'm " + d.age + " years old."
-})
